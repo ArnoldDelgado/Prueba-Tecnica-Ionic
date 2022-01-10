cc8efd86994948d1a1b8b36b32382a882d62b72d
ArnoldDelgado <68920047+ArnoldDelgado@users.noreply.github.com>
2022-01-10 14:38:51 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Healt Food',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
